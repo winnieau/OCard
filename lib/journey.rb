6901da7aa5928f016cfbbac221652199301fc406
Ajitsy <sanghera.p@gmail.com>
2016-02-24 19:31:21 +0000

    knows fare and can charge a penalty

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,5 +1,8 @@
 class Journey
 
+  MINIMUM_FARE = 1
+  PENALTY_FARE = 6
+
   attr_reader :journeys
   attr_accessor :entry_station, :exit_station
 
@@ -21,6 +24,14 @@ class Journey
   def end(exit_station)
     @exit_station = exit_station
     journeys[entry_station] = exit_station
+  end
+
+  def fare (entry_station = false, exit_station = false)
+    if ((entry_station || exit_station) == false)
+      return PENALTY_FARE
+    else
+      MINIMUM_FARE
+    end
   end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,6 +1,7 @@
 require 'journey'
 
 describe Journey do
+
   it {is_expected.to respond_to(:in_journey?)}
 
   it 'has a default of no journeys made' do
@@ -19,6 +20,16 @@ describe Journey do
   it 'can end a journey' do
     subject.end("station")
     expect(subject).not_to be_in_journey
+  end
+
+  it 'should return minimum fare for completed journey' do
+    minimum_fare = Journey::MINIMUM_FARE
+    expect(subject.fare("entry", "exit")).to eq(minimum_fare)
+  end
+
+  it 'should give penalty fare if entry_station or exit_station is missing' do
+    penalty_fare = Journey::PENALTY_FARE
+    expect(subject.fare()).to eq(penalty_fare)
   end
 
 end
